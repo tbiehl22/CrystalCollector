bbab31b12c4b6e3a08a2b10ef993e5b9e821d1e4
Tim Biehl <tbiehl22@gmail.com>
2020-01-20 14:23:40 -0700

    fixed reset js for score to match

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,34 +12,69 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-$(document).ready(function() {
+$(document).ready(function()  {
 
    randomNumberToMatch = getRandomInt(19,120);
-   console.log("Random Number", randomNumberToMatch);
-   $("#matchingScore").text("Match this score:" + randomNumberToMatch);
+   $("#matchingScore").text("Match this score: " + randomNumberToMatch);
 
-   function gameReset() {
-       score = 0;
+   function gameReset() {   
        $("#score").text("Your total score is: " + score);
        randomNumberToMatch = getRandomInt(19,120);
-       console.log("Random Number", randomNumberToMatch);
-       $("matchingScore").text("Match this score: " + randomNumberToMatch);
+       $("#matchingScore").text("Match this score:  " + randomNumberToMatch);
+       score = 0;
        red = 0;
        blue = 0;
        orange = 0;
        purple = 0;
    }
-})
 
-function checkForTheWin() {
-    if (score === randomNumberToMatch) {
+    function checkForTheWin() {
+        if (score === randomNumberToMatch) {
         wins++;
+        alert("Congratulations! You win!")
         $("#wins").text("Wins: " + wins);
         gameReset();
-    }
-    else if (score > randomNumberToMatch) {
-        losses++;
-        $("#losses").text("Losses: " + losses);
-        gameReset();
-    }
-}
+     }
+        else if (score > randomNumberToMatch) {
+            losses++;
+            alert("You lose! Try again!")
+            $("#losses").text("Losses: " + losses);
+            gameReset();
+        }
+     }
+        $("#crystal1").on("click", function() {
+        if (red === 0) {
+         red = getRandomInt(1, 12);
+        }
+        score += red;
+        $("#score").text("Your total score is: " + score);
+        checkForTheWin();
+    });
+
+    $("#crystal2").on("click", function() {
+        if (blue === 0) {
+        blue = getRandomInt(1, 12);
+        }
+        score += blue;
+        $("#score").text("Your total score is: " + score);
+        checkForTheWin();
+    });
+
+    $("#crystal3").on("click", function() {
+        if (orange === 0) {
+        orange = getRandomInt(1, 12);
+        }
+        score += orange;
+        $("#score").text("Your total score is: " + score);
+        checkForTheWin();
+    });
+
+    $("#crystal4").on("click", function() {
+        if (purple === 0) {
+        purple = getRandomInt(1, 12);
+        }
+        score += purple;
+        $("#score").text("Your total score is: " + score);
+        checkForTheWin();
+    });
+});
